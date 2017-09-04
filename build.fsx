53a6b3c1fab8d6a6bdab17366d94b3ff4ea01950
ravengerUA <yurisikorsky@gmail.com>
2017-09-04 09:46:55 +0300

    fix nuget

--- build.fsx
+++ build.fsx
@@ -76,7 +76,7 @@ Target "RunTests" (fun _ ->
 Target "CreateNuget" (fun _ ->
     let envBuildNumber = environVarOrDefault "APPVEYOR_BUILD_NUMBER" "0"
     let branch =  environVarOrDefault "APPVEYOR_REPO_BRANCH" ""
-    let versionSuffix = if branch.Equals("dev") then (sprintf "beta-%s" envBuildNumber) else ""
+    let versionSuffix = if branch.Equals("dev") then (sprintf "beta%s" envBuildNumber) else ""
 
     let projects = !! "./**/Akka.Streams.Kafka.csproj"
 
