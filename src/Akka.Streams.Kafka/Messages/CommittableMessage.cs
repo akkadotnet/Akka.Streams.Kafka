f51fb940d7585ed9941b671cffa962521d65d175
Alex Valuyskiy <alexvaluyskiy@users.noreply.github.com>
2017-09-12 21:11:32 +0300

    Committable Source (#12)
    * Init
    
    * work on tests
    
    * Create CommitableSource_resume_from_commited_offset test
    
    * fix
    
    * fix
    
    * Finished basic implementation
    
    * fixed error handling
    
    * Updated commitable source

---
+++ src/Akka.Streams.Kafka/Messages/CommittableMessage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Akka.Streams.Kafka.Messages
+{
+    public sealed class CommittableMessage<K, V>
+    {
+        public CommittableMessage(Message<K, V> record, CommitableOffset commitableOffset)
+        {
+            Record = record;
+            CommitableOffset = commitableOffset;
+        }
+
+        public Message<K, V> Record { get; }
+
+        public CommitableOffset CommitableOffset { get; }
+    }
+
+    public class CommitableOffset
+    {
+        private readonly Func<Task<CommittedOffsets>> _task;
+
+        public CommitableOffset(Func<Task<CommittedOffsets>> task, PartitionOffset offset)
+        {
+            _task = task;
+            Offset = offset;
+        }
+
+        public PartitionOffset Offset { get; }
+
+        public Task<CommittedOffsets> Commit()
+        {
+            return _task();
+        }
+    }
+
+    public class PartitionOffset
+    {
+        public PartitionOffset(string groupId, string topic, int partition, Offset offset)
+        {
+            GroupId = groupId;
+            Topic = topic;
+            Partition = partition;
+            Offset = offset;
+        }
+
+        public string GroupId { get; }
+
+        public string Topic { get; }
+
+        public int Partition { get; }
+
+        public Offset Offset { get; }
+    }
+}
