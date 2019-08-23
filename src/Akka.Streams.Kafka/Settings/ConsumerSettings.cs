f6e72224581f905e08feb953cc561e5b57111c28
IgorFedchenko <igor@fedchenko.pro>
2019-08-23 11:45:23 +0300

    Not set groupId should not show initialized value

--- src/Akka.Streams.Kafka/Settings/ConsumerSettings.cs
+++ src/Akka.Streams.Kafka/Settings/ConsumerSettings.cs
@@ -68,7 +68,7 @@ namespace Akka.Streams.Kafka.Settings
 
         public ConsumerSettings<TKey, TValue> WithDispatcher(string dispatcherId) => Copy(dispatcherId: dispatcherId);
 
-        public string GroupId => Properties.ContainsKey("group.id") ? Properties["group.id"] : string.Empty;
+        public string GroupId => Properties.ContainsKey("group.id") ? Properties["group.id"] : null;
 
         private ConsumerSettings<TKey, TValue> Copy(
             IDeserializer<TKey> keyDeserializer = null,
