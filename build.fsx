6657638223fd950cfe2f0e7a89ebe92eb05a93bc
Aaron Stannard <aaron@petabridge.com>
2020-02-18 17:40:39 -0600

    updated build system to support betas

--- build.fsx
+++ build.fsx
@@ -23,15 +23,21 @@ let solutionFile = FindFirstMatchingFile "*.sln" __SOURCE_DIRECTORY__  // dynami
 let buildNumber = environVarOrDefault "BUILD_NUMBER" "0"
 let hasTeamCity = (not (buildNumber = "0")) // check if we have the TeamCity environment variable for build # set
 let preReleaseVersionSuffix = "beta" + (if (not (buildNumber = "0")) then (buildNumber) else DateTime.UtcNow.Ticks.ToString())
+
+let releaseNotes =
+    File.ReadLines (__SOURCE_DIRECTORY__ @@ "RELEASE_NOTES.md")
+    |> ReleaseNotesHelper.parseReleaseNotes
+
+let versionFromReleaseNotes =
+    match releaseNotes.SemVer.PreRelease with
+    | Some r -> r.Origin
+    | None -> ""
+
 let versionSuffix = 
     match (getBuildParam "nugetprerelease") with
     | "dev" -> preReleaseVersionSuffix
-    | "" -> ""
+    | "" -> versionFromReleaseNotes
     | str -> str
-
-let releaseNotes =
-    File.ReadLines "./RELEASE_NOTES.md"
-    |> ReleaseNotesHelper.parseReleaseNotes
 
 // Directories
 let toolsDir = __SOURCE_DIRECTORY__ @@ "tools"
