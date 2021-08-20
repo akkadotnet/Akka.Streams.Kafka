719b7e6060a201839ce73b63d8a096595fd7d5f6
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-08-21 05:30:50 +0700

    Add benchmark

---
+++ src/Akka.Streams.Kafka.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Akka.Streams.Kafka.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<PlainSinkConsumerBenchmark>();
+        }
+    }
+}
