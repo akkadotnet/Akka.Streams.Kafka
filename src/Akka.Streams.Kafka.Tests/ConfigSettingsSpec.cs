49314858e2f1f005069d41cafa4e12921f350b1d
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-11-19 01:33:51 +0700

    Add convenience methods and code cleanup (#260)
    * Add convenience methods to settings class
    
    * Make log messages more consistent and concise

--- src/Akka.Streams.Kafka.Tests/ConfigSettingsSpec.cs
+++ src/Akka.Streams.Kafka.Tests/ConfigSettingsSpec.cs
@@ -1,5 +1,6 @@
 ﻿using Akka.Configuration;
 using Akka.Streams.Kafka.Settings;
+using Confluent.Kafka;
 using FluentAssertions;
 using Xunit;
 
@@ -8,13 +9,15 @@ namespace Akka.Streams.Kafka.Tests
     public class ConfigSettingsSpec
     {
         [Fact]
-        public void ConfigSettings_must_handleNestedKafkaClientsProperties()
+        public void ConsumerSettings_must_handleNestedKafkaClientsProperties()
         {
             var conf = ConfigurationFactory.ParseString(@"
-                akka.kafka.consumer.kafka-clients.bootstrap.servers = ""localhost:9092""
-                akka.kafka.consumer.kafka-clients.bootstrap.foo = baz
-                akka.kafka.consumer.kafka-clients.foo = bar
-                akka.kafka.consumer.kafka-clients.client.id = client1
+akka.kafka.consumer.kafka-clients {{
+    bootstrap.servers = ""localhost:9092""
+    bootstrap.foo = baz
+    foo = bar
+    client.id = client1
+}}
             ").WithFallback(KafkaExtensions.DefaultSettings).GetConfig("akka.kafka.consumer");
 
             var settings = ConsumerSettings<string, string>.Create(conf, null, null);
@@ -22,6 +25,67 @@ namespace Akka.Streams.Kafka.Tests
             settings.GetProperty("client.id").Should().Be("client1");
             settings.GetProperty("foo").Should().Be("bar");
             settings.GetProperty("bootstrap.foo").Should().Be("baz");
+            settings.GetProperty("enable.auto.commit").Should().Be("false");
         }
+        
+        [Fact]
+        public void ConsumerSettings_must_beAbleToMergeConsumerConfig()
+        {
+            var conf = KafkaExtensions.DefaultSettings.GetConfig("akka.kafka.consumer");
+            var settings = ConsumerSettings<string, string>.Create(conf, null, null);
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Latest,
+                EnableAutoCommit = true,
+                GroupId = "group1",
+                ClientId = "client1"
+            };
+
+            settings = settings.WithConsumerConfig(config);
+            settings.GetProperty("bootstrap.servers").Should().Be("localhost:9092");
+            settings.GetProperty("auto.offset.reset").Should().Be("latest");
+            settings.GetProperty("enable.auto.commit").Should().Be("True");
+            settings.GetProperty("group.id").Should().Be("group1");
+            settings.GetProperty("client.id").Should().Be("client1");
+        }
+        
+        [Fact]
+        public void ProducerSettings_must_handleNestedKafkaClientsProperties()
+        {
+            var conf = ConfigurationFactory.ParseString(@"
+akka.kafka.producer.kafka-clients {{
+    bootstrap.servers = ""localhost:9092""
+    bootstrap.foo = baz
+    foo = bar
+    client.id = client1
+}}
+            ").WithFallback(KafkaExtensions.DefaultSettings).GetConfig("akka.kafka.producer");
+
+            var settings = ProducerSettings<string, string>.Create(conf, null, null);
+            settings.GetProperty("bootstrap.servers").Should().Be("localhost:9092");
+            settings.GetProperty("client.id").Should().Be("client1");
+            settings.GetProperty("foo").Should().Be("bar");
+            settings.GetProperty("bootstrap.foo").Should().Be("baz");
+        }
+        
+        [Fact]
+        public void ProducerSettings_must_beAbleToMergeProducerConfig()
+        {
+            var conf = KafkaExtensions.DefaultSettings.GetConfig("akka.kafka.producer");
+            var settings = ProducerSettings<string, string>.Create(conf, null, null);
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = "client1", 
+                EnableIdempotence = true
+            };
+
+            settings = settings.WithProducerConfig(config);
+            settings.GetProperty("bootstrap.servers").Should().Be("localhost:9092");
+            settings.GetProperty("client.id").Should().Be("client1");
+            settings.GetProperty("enable.idempotence").Should().Be("True");
+        }
+        
     }
 }
