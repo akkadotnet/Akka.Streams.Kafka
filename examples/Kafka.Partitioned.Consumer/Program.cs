60cd56d7437614b2501f51753887632a6748163d
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-03-22 03:03:25 +0700

    Add manual partition sample (#286)
    * Add manual partition sample
    
    * convert framework target from net6.0 to netcoreapp3.1
    
    * remove test code

---
+++ examples/Kafka.Partitioned.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Streams.Kafka.Settings;
+using Kafka.Partitioned.Consumer.Actors;
+
+namespace Kafka.Partitioned.Consumer
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+                    akka.suppress-json-serializer-warning=true
+                    akka.loglevel = INFO
+                ").WithFallback(KafkaExtensions.DefaultSettings);
+
+            var system = ActorSystem.Create("TestKafka", config);
+            var consumerSettings = ConsumerSettings<string, string>.Create(system, null, null)
+                .WithBootstrapServers("localhost:29092")
+                .WithGroupId("group1")
+                .WithProperty("session.timeout.ms", "6000");
+            var subscription = Subscriptions.Topics("akka100");
+            system.ActorOf(KafkaConsumerSupervisor<string, string>.Props(consumerSettings, subscription, 3), "kafka");
+
+            Console.WriteLine("Press any key to stop consumer.");
+            Console.ReadKey();
+
+            await system.Terminate();
+        }
+    }
+}
