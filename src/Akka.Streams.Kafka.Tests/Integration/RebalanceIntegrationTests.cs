180e89eed41dadb7bc0f8490f1f37bd07da60999
Aaron Stannard <aaron@petabridge.com>
2025-03-10 21:17:13 -0500

    have a failing test
    filtering has not been implemented yet in sub-sources

--- src/Akka.Streams.Kafka.Tests/Integration/RebalanceIntegrationTests.cs
+++ src/Akka.Streams.Kafka.Tests/Integration/RebalanceIntegrationTests.cs
@@ -12,6 +12,7 @@ using Akka.Streams.Kafka.Messages;
 using Akka.Streams.Kafka.Settings;
 using Akka.Streams.TestKit;
 using Akka.Util;
+using Akka.Util.Internal;
 using Confluent.Kafka;
 using FluentAssertions;
 using FluentAssertions.Extensions;
@@ -30,40 +31,6 @@ public class RebalanceIntegrationTests : KafkaIntegrationTests
     private static readonly IReadOnlyList<int> Numbers = Enumerable.Range(0, 5000).ToList();
     private const string ConsumerClientId1 = "consumer-1";
     private const string ConsumerClientId2 = "consumer-2";
-
-    private static Source<ConsumeResult<Null, string>, IControl> GetConsumer(
-        ConsumerSettings<Null, string> settings,
-        string topic)
-    {
-        return KafkaConsumer.PlainSource(settings, Subscriptions.Topics(topic));
-    }
-
-    private sealed class StreamCompleted
-    {
-        public static StreamCompleted Instance { get; } = new();
-
-        private StreamCompleted()
-        {
-        }
-    }
-
-    private sealed record StreamFailed(Exception Ex);
-
-    private IKillSwitch CreateKillableStream(string topic, ConsumerSettings<Null, string> settings, IActorRef sinkRef,
-        string consumerName)
-    {
-        var source = GetConsumer(settings, topic);
-        var killSwitch = source
-            .WithAttributes(Attributes.CreateName(consumerName))
-            .ViaMaterialized(KillSwitches.Single<ConsumeResult<Null, string>>(), Keep.Right)
-            .WithAttributes(Attributes.CreateName($"selectAsync-{consumerName}"))
-            .ToMaterialized(
-                Sink.ActorRef<ConsumeResult<Null, string>>(sinkRef, StreamCompleted.Instance,
-                    exception => new StreamFailed(exception)), Keep.Left)
-            .Run(Materializer.WithNamePrefix(consumerName));
-
-        return killSwitch;
-    }
 
     /// <summary>
     /// Reproduction spec for https://github.com/akkadotnet/Akka.Streams.Kafka/issues/415
@@ -132,22 +99,140 @@ public class RebalanceIntegrationTests : KafkaIntegrationTests
         _ = probe2.RequestAsync(count);
 
         // winning node is whoever was assigned to partition1
-        var (winningConsumer, losingConsumer) 
+        var (winningConsumer, losingConsumer)
             = assignedPartitionConsumer1 == tp1 ? (probe1, probe2) : (probe2, probe1);
-        
+
         Log.Debug("Read all messages from winning consumer");
         var winningMessages = await winningConsumer.ExpectNextNAsync(count).ToListAsync();
-        
+
         Log.Debug("Expect no messages on losing consumer");
         await losingConsumer.ExpectNoMsgAsync(TimeSpan.FromMilliseconds(500));
-        
+
         Assert.Equal(count, winningMessages.Count);
 
+        await probe1.CancelAsync();
+        await probe2.CancelAsync();
+
+        // TODO: bug - if we've cancelled the stream, the controls should complete automatically
+        // Assert.True(control1.IsShutdown.IsCompleted);
+        // Assert.True(control2.IsShutdown.IsCompleted);
+    }
+
+    [Fact]
+    public async Task FetchedRecords_must_be_removed_from_the_partitioned_source_stage_when_a_partition_is_revoked()
+    {
+        const int count = 20;
+        var topic = CreateTopic(1);
+        var group = CreateGroup(1);
+        var tp0 = new TopicPartition(topic, 0);
+        var tp1 = new TopicPartition(topic, 1);
+
+        await GivenInitializedTopicAsync(tp1, 2);
+
+        var settings = CreateConsumerSettings<Null, string>(group);
+
+        await ProduceStrings(tp1, Enumerable.Range(0, count), ProducerSettings);
+
+        Log.Debug("Subscribe to the topic (without downstream demand)");
+        var probe1RebalanceActor = CreateTestProbe();
+        var probe1Subscription = Subscriptions.Topics(topic).WithRebalanceListener(probe1RebalanceActor.Ref);
+        var (control1, probe1) = KafkaConsumer
+            .PlainPartitionedSource(settings.WithClientId(ConsumerClientId1), probe1Subscription)
+            .ToMaterialized(this.SinkProbe<(TopicPartition, Source<ConsumeResult<Null, string>, NotUsed>)>(), Keep.Both)
+            .Run(Materializer);
+
+        Log.Debug("Await initial partition assignment");
+        (await probe1RebalanceActor.ExpectMsgAsync<TopicPartitionsAssigned>()).Partitions.Should()
+            .BeEquivalentTo([tp0, tp1]);
+
+        Log.Debug("Read 2 sub-sources returned by the partitioned source");
+        await probe1.RequestAsync(2);
+
+        var probe1RunningSubSourceProbes = await SubSourcesWithProbes(2, probe1);
+
+        Log.Debug("Read one message from probe1 with partition 1");
+        probe1RunningSubSourceProbes.Where(c => c.Item1.Partition == 1)
+            .ForEach(c => c.Item2.RequestNext());
+
+        Log.Debug("Subscribe to the topic (without downstream demand)");
+        var probe2RebalanceActor = CreateTestProbe();
+        var probe2Subscription = Subscriptions.Topics(topic).WithRebalanceListener(probe2RebalanceActor.Ref);
+        var (control2, probe2) = KafkaConsumer
+            .PlainPartitionedSource(settings.WithClientId(ConsumerClientId2), probe2Subscription)
+            .ToMaterialized(this.SinkProbe<(TopicPartition, Source<ConsumeResult<Null, string>, NotUsed>)>(), Keep.Both)
+            .Run(Materializer);
+
+        await probe2.RequestAsync(1);
+        var probe2RunningSubSourceProbes = await SubSourcesWithProbes(1, probe2);
+
+        // All partitions should be revoked from consumer 1
+        Log.Debug("Await a revoke to consumer 1");
+        var revokedPartitions = (await probe1RebalanceActor.ExpectMsgAsync<TopicPartitionsRevoked>()).Partitions;
+        Log.Debug("Revoked partitions [{0}] from consumer 1", revokedPartitions);
+
+        // Single partition should be assigned back to both consumers
+        Log.Debug("Await assign to consumer 2");
+        var assignedPartitionConsumer2 =
+            (await probe2RebalanceActor.ExpectMsgAsync<TopicPartitionsAssigned>()).Partitions.Single();
+
+        Log.Debug("Awaiting assign to consumer 1");
+        var assignedPartitionConsumer1 =
+            (await probe1RebalanceActor.ExpectMsgAsync<TopicPartitionsAssigned>()).Partitions.Single();
+
+        // sanity check
+        Assert.NotEqual(assignedPartitionConsumer1, assignedPartitionConsumer2);
+
+        // get the winning and losing consumers
+        var (winningConsumer, losingConsumer)
+            = assignedPartitionConsumer1 == tp1
+                ? (probe1RunningSubSourceProbes, probe2RunningSubSourceProbes)
+                : (probe2RunningSubSourceProbes, probe1RunningSubSourceProbes);
+
+        Log.Debug("Resume demand on both consumers");
+        RunForSubSource(1, probe1RunningSubSourceProbes, c => c.Request(count));
+        RunForSubSource(1, probe2RunningSubSourceProbes, c => c.Request(count));
+
+        Log.Debug("No further messages should be emitted from the losing consumer");
+        RunForSubSource(1, losingConsumer, c => c.ExpectComplete());
+
+        var winningMessages = winningConsumer
+            .Where(c => c.Item1.Partition.Value == 1)
+            .SelectMany(c => c.Item2.ExpectNextN(count))
+            .ToList();
+        
+        Assert.Equal(count, winningMessages.Count);
+        
         await probe1.CancelAsync();
         await probe2.CancelAsync();
         
         // TODO: bug - if we've cancelled the stream, the controls should complete automatically
         // Assert.True(control1.IsShutdown.IsCompleted);
         // Assert.True(control2.IsShutdown.IsCompleted);
+
+        return;
+
+        ValueTask<List<(TopicPartition, TestSubscriber.Probe<ConsumeResult<Null, string>>)>> SubSourcesWithProbes(
+            int partitions,
+            TestSubscriber.Probe<(TopicPartition, Source<ConsumeResult<Null, string>, NotUsed>)> probe)
+        {
+            return probe.ExpectNextNAsync(partitions)
+                .Select(c =>
+                {
+                    var (tp, source) = c;
+                    var subProbe = source.RunWith(this.SinkProbe<ConsumeResult<Null, string>>(), Materializer);
+                    return (tp, subProbe);
+                }).ToListAsync();
+        }
+
+        void RunForSubSource(int partition,
+            List<(TopicPartition, TestSubscriber.Probe<ConsumeResult<Null, string>>)> subSourcesWithProbes,
+            Action<TestSubscriber.Probe<ConsumeResult<Null, string>>> fun)
+        {
+            foreach (var (_, probe) in subSourcesWithProbes
+                         .Where(c => c.Item1.Partition == partition))
+            {
+                fun(probe);
+            }
+        }
     }
 }
