361b54e4e501ec27c7f04c928a229d60d635cc0f
Aaron Stannard <aaron@petabridge.com>
2025-03-16 21:57:53 -0500

    delete Docker.NET junk

--- src/Akka.Streams.Kafka.Benchmark/BenchmarkBase.cs
+++ src/Akka.Streams.Kafka.Benchmark/BenchmarkBase.cs
@@ -1,46 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Configuration;
-using Akka.Streams.Dsl;
-using Akka.Streams.Kafka.Dsl;
-using Akka.Streams.Kafka.Messages;
 using Akka.Streams.Kafka.Settings;
-using Akka.Util;
-using BenchmarkDotNet.Attributes;
-using Confluent.Kafka;
-using Docker.DotNet;
-using Docker.DotNet.Models;
-using Config = Akka.Configuration.Config;
-using DockerConfig = Docker.DotNet.Models.Config;
 
 namespace Akka.Streams.Kafka.Benchmark
 {
     public abstract class BenchmarkBase
     {
-        public readonly DockerSupport Docker;
+        protected const string KafkaBootstrapServers = "localhost:29092";
         public ActorSystem ActorSystem { get; private set; } = null!;
         
         protected BenchmarkBase()
         {
-            Docker = new DockerSupport();
         }
         
-        public async Task SetupKafkaAsync()
+        public Task SetupKafkaAsync()
         {
             _uuid = Guid.NewGuid().ToString();
             KafkaTopic = $"topic-1-{_uuid}";
             KafkaGroup = $"group-1-{_uuid}";
-            
-            await Docker.SetupContainersAsync();
-            await Docker.WaitForKafkaServerAsync();
+            return Task.CompletedTask;
         }
 
         public async Task SetupAkkaAsync()
@@ -53,15 +33,14 @@ namespace Akka.Streams.Kafka.Benchmark
             await TeardownActorSystemsAsync();
         }
         
-        public async Task TearDownKafkaAsync()
+        public Task TearDownKafkaAsync()
         {
-            await Docker.TearDownDockerAsync();
+            return Task.CompletedTask;
         }
 
         #region Akka methods
 
         private string _uuid = null!;
-        //public ActorSystem ProducerSystem { get; private set; }
         public ActorSystem ConsumerSystem { get; private set; } = null!;
         public string KafkaTopic { get; private set; } = null!;
         public string KafkaGroup { get; private set; } = null!;
@@ -70,7 +49,6 @@ namespace Akka.Streams.Kafka.Benchmark
         {
             Console.WriteLine("Starting Akka ActorSystems");
             
-            //var config = ConfigurationFactory.ParseString("akka.loglevel = DEBUG");
             var config = ConfigurationFactory.ParseString("""
                                                           
                                                                     akka {
@@ -90,35 +68,13 @@ namespace Akka.Streams.Kafka.Benchmark
                                                           """)
                 .WithFallback(KafkaExtensions.DefaultSettings);
             
-            //ProducerSystem = ActorSystem.Create("akka-kafka-producer", config);
             ConsumerSystem = ActorSystem.Create("akka-kafka-consumer", config);
             Console.WriteLine("ActorSystems started");
-            
-            /*
-            var producerSettings = ProducerSettings<Null, string>
-                .Create(ProducerSystem, null, null)
-                .WithBootstrapServers(Docker.KafkaAddress);
-            
-            Console.WriteLine("Creating Kafka messages");
-            // Prime kafka with messages
-            Source
-                .From(Enumerable.Range(1, 100000))
-                .Select(elem => new ProducerRecord<Null, string>(KafkaTopic, elem.ToString()))
-                .RunWith(KafkaProducer.PlainSink(producerSettings), ProducerSystem.Materializer());
-                
-            Console.WriteLine("ActorSystems created");
-            */
-            
             return Task.CompletedTask;
         }
 
         private async Task TeardownActorSystemsAsync()
         {
-            /*
-            await Task.WhenAll(
-                ProducerSystem.Terminate(),
-                ConsumerSystem.Terminate());
-                */
             try
             {
                 await ConsumerSystem.Terminate();
@@ -130,6 +86,5 @@ namespace Akka.Streams.Kafka.Benchmark
         }
 
         #endregion
-
     }
 }
