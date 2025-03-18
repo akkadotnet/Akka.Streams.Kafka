ae66bd258e3a7448ab5e98eee83bf3def6d0dc08
Aaron Stannard <aaron@petabridge.com>
2025-03-17 22:28:57 -0500

    Update CommitTimeoutException.cs

--- src/Akka.Streams.Kafka/Stages/Consumers/Exceptions/CommitTimeoutException.cs
+++ src/Akka.Streams.Kafka/Stages/Consumers/Exceptions/CommitTimeoutException.cs
@@ -3,7 +3,7 @@ using System;
 namespace Akka.Streams.Kafka.Stages.Consumers.Exceptions
 {
     /// <summary>
-    /// Commita attempts will be failed with this exception if
+    /// Commit attempts will be failed with this exception if
     /// Kafka doesn't respond within commit timeout
     /// </summary>
     public sealed class CommitTimeoutException : TimeoutException
