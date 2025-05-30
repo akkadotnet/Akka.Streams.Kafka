aff02f73f81f70a0c31bfabc9e9be21a5b58095c
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-05-30 21:19:42 +0700

    Code cleanup

--- examples/Simple/SimpleConsumer.AppHost/Stub.cs
+++
@@ -1,7 +0,0 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="Stub.cs" company="Akka.NET Project">
-//      Copyright (C) 2025 - 2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
-// </copyright>
-// -----------------------------------------------------------------------
-
-namespace Akka.Event;
